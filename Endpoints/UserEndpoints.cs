ef826235a9d44d4ecb010ee70f5dbb9cee9baf9a
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-06-02 23:17:56 -0300

    login endpoint created

--- Endpoints/UserEndpoints.cs
+++ Endpoints/UserEndpoints.cs
@@ -28,6 +28,23 @@ public static class UserEndpoints
                 return Results.Created("user/signin", newUserJwt);
             }
         );
+        group.MapPost("login", async ([FromServices] UserController userController, [FromBody] UserLoginRequestModel userLoginRequestModel) =>
+        {
+            string userJwt;
+            try
+            {
+                userJwt = await userController.LoginUser(userLoginRequestModel);
+            }
+            catch (UserNotFoundException ex)
+            {
+                return Results.NotFound(ex.Message);
+            }
+            catch (Exception ex) when (ex is InvalidRequestInfoException or WrongUserPasswordException)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+            return Results.Ok(userJwt);
+        });
 
         return group;
     }
